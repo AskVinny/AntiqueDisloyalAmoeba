d636d796624ef63b2a2f9f4136e701ff4370c774
AskVinny <164677857+AskVinny@users.noreply.github.com>
2024-03-24 19:29:49 +0000

    done

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,8 @@ const axios = require("axios");
 const corsHandler = cors({ origin: true });
 
 exports.addEmailToAirtable = functions.https.onRequest((request, response) => {
-  corsHandler(request, response, () => { // Wrap the entire function logic within the CORS middleware
+  corsHandler(request, response, () => {
+    // Wrap the entire function logic within the CORS middleware
     // Proceed only if it's a POST request; otherwise, respond with 405 for non-OPTIONS, non-POST requests
     if (request.method === "POST") {
       const { email, timestamp } = request.body;
@@ -14,7 +15,9 @@ exports.addEmailToAirtable = functions.https.onRequest((request, response) => {
       // Define the Axios request config
       const config = {
         method: "post",
-        url: `https://api.airtable.com/v0/${functions.config().airtable.base}/tbl3Mm0Exzo8eUkPg`,
+        url: `https://api.airtable.com/v0/${
+          functions.config().airtable.base
+        }/tbl3Mm0Exzo8eUkPg`,
         headers: {
           Authorization: `Bearer ${functions.config().airtable.key}`,
           "Content-Type": "application/json",
@@ -29,15 +32,16 @@ exports.addEmailToAirtable = functions.https.onRequest((request, response) => {
 
       // Attempt to send data to Airtable
       axios(config)
-        .then(res => response.status(200).send({ data: res.data }))
-        .catch(error => {
+        .then((res) => response.status(200).send({ data: res.data }))
+        .catch((error) => {
           console.error("Error adding to Airtable:", error);
           response.status(500).send({ error: "Failed to add to Airtable." });
         });
     } else if (request.method === "OPTIONS") {
       // OPTIONS requests are automatically handled by Firebase; no need for explicit handling
-      response.status(204).send('');
+      response.status(204).send("");
     } else {
+      console.log("Invalid request method:", request.method);
       // Method not allowed
       response.status(405).send("Method Not Allowed");
     }
