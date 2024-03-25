24578b7650a549051f45d18774776d57bc817723
AskVinny <164677857+AskVinny@users.noreply.github.com>
2024-03-25 10:15:34 +0000

    Setting the correct headers
    Changing headers to just ones we need

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,49 @@
 const { onRequest } = require("firebase-functions/v2/https");
+const axios = require("axios");
+const { defineString } = require('firebase-functions/params');
+
+
+const airtableTable = defineString('AIRTABLE_TABLE_KEY');
+const airtableBase = defineString('AIRTABLE_BASE_KEY');
+const airtableSecret = defineString('AIRTABLE_API_KEY');
 
 exports.addEmailToAirtable = onRequest(
-  { cors: true },
+  { cors: { origin: true, methods: ["POST"] } }, // Enable CORS for POST requests
   (req, res) => {
-  res.status(200).send({ message: "Hello world!" });
-  }
+    // Proceed only if it's a POST request; otherwise, end the request
+    if (req.method !== "POST") {
+      res.status(405).send({ error: "Method Not Allowed" });
+      return;
+    }
+
+    // Extract email and timestamp from the request body
+    const { email, timestamp } = req.body;
+
+    // Define the Axios request config for Airtable API
+    const config = {
+      method: "post",
+      url: `https://api.airtable.com/v0/${
+        airtableBase.value()
+      }/${airtableTable.value()}`,
+      headers: {
+        Authorization: `Bearer ${airtableSecret.value()}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        fields: {
+          Email: email,
+          Timestamp: timestamp,
+        },
+      },
+    };
+    // Attempt to send data to Airtable using Axios
+    axios(config)
+      .then((airtableResponse) => {
+        res.status(200).send({ data: airtableResponse.data });
+      })
+      .catch((error) => {
+        console.error("Error adding to Airtable:", error);
+        res.status(500).send({ error: "Failed to add to Airtable." });
+      });
+  },
 );
